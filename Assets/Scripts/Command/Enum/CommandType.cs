aed2b11df4b5f36b0d4069e53a2ce9e44a0475b8
yokotarina <yokota.rina@moonshot-apollo.com>
2022-04-28 15:12:52 +0900

    差分更新②

---
+++ Assets/Scripts/Command/Enum/CommandType.cs
@@ -0,0 +1,46 @@
+using Utils;
+
+namespace Command.Enum
+{
+    /// <summary>
+    /// コマンドの種類
+    /// </summary>
+    public enum CommandType
+    {
+        [Display(Name = "指定なし")]
+        None = 0,
+
+        [Display(Name = "テキスト表示")]
+        Text = 1,
+
+        [Display(Name = "キャラクター表示")]
+        Character = 2,
+
+        [Display(Name = "背景表示")]
+        BackGround = 3,
+
+        [Display(Name = "ボイス再生")]
+        Voice = 4,
+
+        [Display(Name = "BGM再生")]
+        BGM = 5,
+
+        [Display(Name = "選択肢")]
+        Select = 6,
+
+        [Display(Name = "選択肢終了")]
+        SelectEnd = 7,
+
+        [Display(Name = "選択肢の分岐地点")]
+        SelectPoint = 8,
+
+        [Display(Name = "ジャンプ")]
+        Jump = 9,
+
+        [Display(Name = "ジャンプ先地点")]
+        JumpPoint = 10,
+
+        [Display(Name = "コメント")]
+        Comment = 99,
+    }
+}
---
+++ Assets/Scripts/Command/Enum/SelectSubCommandType.cs
@@ -0,0 +1,28 @@
+using Utils;
+
+namespace Command.Enum
+{
+    /// <summary>
+    /// 選択肢のサブコマンドの種類
+    /// </summary>
+    public enum SelectSubCommandType
+    {
+        [Display(Name = "指定なし")]
+        None = 0,
+
+        [Display(Name = "選択肢テキスト")]
+        ChoiceWord = 1,
+
+        [Display(Name = "テキスト表示")]
+        Text = 2,
+
+        [Display(Name = "キャラクター表示")]
+        Character = 3,
+
+        [Display(Name = "ボイス再生")]
+        Voice = 4,
+
+        [Display(Name = "エフェクト再生")]
+        BGM = 5,
+    }
+}
